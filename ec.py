a89b20fe8dd1d5b580dd5841e2980f07ad88b1ef
Ken Kundert <thenurd@nurdletech.com>
2013-08-27 15:56:09 -0700

    Added GPLv3 license.

--- ec.py
+++ ec.py
@@ -1,8 +1,23 @@
 #!/usr/bin/env python
 #
-# Engineering Calculator
+# Engineering Calculator {{{1
 #
 # An RPN calculator that supports numbers with SI scale factors and units.
+#
+# Copyright (C) 2013 Kenneth S. Kundert
+#
+# This program is free software: you can redistribute it and/or modify
+# it under the terms of the GNU General Public License as published by
+# the Free Software Foundation, either version 3 of the License, or
+# (at your option) any later version.
+#
+# This program is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+# GNU General Public License for more details.
+#
+# You should have received a copy of the GNU General Public License
+# along with this program.  If not, see [http://www.gnu.org/licenses/].
 
 # Imports {{{1
 from __future__ import division
