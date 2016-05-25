722a22b5401fbce56cf579c46b745fa5bd6789aa
Ken Kundert <thenurd@nurdletech.com>
2016-05-25 16:18:20 -0700

    Bump version: 1.1.5 → 1.1.6

--- calculator.py
+++ calculator.py
@@ -16,7 +16,7 @@ from pydoc import pager
 import sys
 
 # Set the version information {{{1
-versionNumber = '1.1.5'
+versionNumber = '1.1.6'
 versionDate = '2016-05-25'
 
 # Utility functions {{{1
--- setup.py
+++ setup.py
@@ -17,7 +17,7 @@ def contents(fname):
 
 setup(
     name='engineering-calculator'
-  , version="1.1.5"
+  , version="1.1.6"
   , description='engineering calculator'
   , long_description=contents('README.rst')
   , author="Ken Kundert"
