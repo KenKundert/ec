2206b3c4df4ccf7dfb84e3648c92f51327be3b97
Ken Kundert <thenurd@nurdletech.com>
2016-09-29 02:01:32 -0700

    Bump version: 1.1.12 → 1.1.13

--- calculator.py
+++ calculator.py
@@ -16,7 +16,7 @@ from pydoc import pager
 import sys
 
 # Set the version information {{{1
-versionNumber = '1.1.12'
+versionNumber = '1.1.13'
 versionDate = '2016-05-25'
 
 # Utility functions {{{1
