afc919275e25a40711ea095c1c1c8f51be06484e
Ken Kundert <thenurd@nurdletech.com>
2016-09-30 01:38:55 -0700

    Fix bug in bumper.

--- setup.py
+++ setup.py
@@ -48,7 +48,7 @@ requires=[
 
 setup(
     name='engineering-calculator',
-    version="1.1.15",
+    version='1.2.2',
     description='engineering calculator',
     long_description=contents('README.rst'),
     author="Ken Kundert",
