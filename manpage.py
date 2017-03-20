51060b5b31c688d63baf45a9e8849d616cddb51f
Ken Kundert <ken@theKunderts.net>
2017-03-19 18:56:52 -0700

    Fixed manpage generation issue in python2.

--- manpage.py
+++ manpage.py
@@ -873,8 +873,9 @@ def write(manFileName, rstFileName=None):
             outputFile.write(rst)
 
     # Generate man page
-    with open(manFileName, 'w') as outputFile:
-        outputFile.write(publish_string(rst, writer=manpage.Writer()).decode())
+    with open(manFileName, 'wb') as outputFile:
+        text = publish_string(rst, writer=manpage.Writer())
+        outputFile.write(text)
 
 if __name__ == '__main__':
     write('ec.1', 'ec.rst')
