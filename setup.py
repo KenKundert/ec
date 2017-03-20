402c50667b6135830f0145db29ff65fa939cf036
Ken Kundert <ken@theKunderts.net>
2017-03-19 19:03:12 -0700

    Update list of supported python versions.

--- setup.py
+++ setup.py
@@ -79,6 +79,7 @@ setup(
       'Programming Language :: Python :: 3.3',
       'Programming Language :: Python :: 3.4',
       'Programming Language :: Python :: 3.5',
+      'Programming Language :: Python :: 3.6',
       'Topic :: Scientific/Engineering',
     ],
     license='GPLv3',
