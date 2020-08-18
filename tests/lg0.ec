dbad4f3217d56fc8f1734d1e98f521d2f10abec4
Ken Kundert <ken@theKunderts.net>
2020-08-17 19:43:05 -0700

    Fix tests.

--- tests/lg0.ec
+++
@@ -1,7 +0,0 @@
-# Constans for Loop Gain Calculations
-
-88.3u "V/per" =Kdet   # gain of phase detector
-9.07G "Hz/V" =Kvco    # gain of voltage controlled oscillator
-2 =M                  # divide ratio of divider at output of VCO
-8 =N                  # divide ratio of main divider
-2 =F                  # divide ratio of prescaler
--- tests/test_scripting.py
+++ tests/test_scripting.py
@@ -48,6 +48,56 @@ testCases = [
     },
 ]
 
+# Scripts {{{1
+scripts = {
+    "lg0.ec": dedent("""
+        # Constants for Loop Gain Calculations
+
+        88.3u "V/per" =Kdet   # gain of phase detector
+        9.07G "Hz/V" =Kvco    # gain of voltage controlled oscillator
+        2 =M                  # divide ratio of divider at output of VCO
+        8 =N                  # divide ratio of main divider
+        2 =F                  # divide ratio of prescaler
+    """).lstrip(),
+    "lg.ec": dedent(r"""
+        # computes and displays loop gain
+        # x register is taken to be frequency
+        =freq
+        88.3u "V/per" =Kdet   # gain of phase detector
+        9.07G "Hz/V" =Kvco    # gain of voltage controlled oscillator
+        2 =M                  # divide ratio of divider at output of VCO
+        8 =N                  # divide ratio of main divider
+        2 =F                  # divide ratio of prescaler
+        freq 2pi* "rads/s" =omega
+        Kdet Kvco* omega/ M/ =a
+        N F* =f
+        a f* =T
+        `Open loop gain = $a\nFeedback factor = $f\nLoop gain = $T`
+        quit
+    """).lstrip(),
+    "compute-zo": dedent("""
+        freq to_jomega           # enter 10MHz and convert to radial freq.
+        Cin * recip              # enter 1nF, multiply by 𝑥 and reciprocate
+                                 # to compute impedance of capacitor at 10MHz
+        Rl ||                    # enter 50 Ohms and compute impedance of
+                                 # parallel combination
+        "Ω" =Zo                  # apply units of Ω and save to Zo
+        ph                       # compute the phase of impedance
+        Zo mag                   # recall complex impedance from Zo and compute its magnitude
+        `Zo = $0 ∠ $1 @ $freq.`  # display the magnitude and phase of Zo
+        quit
+    """).lstrip(),
+    ".ecrc": dedent("""
+        # define default values for parameters
+        10MHz =freq   # operating frequency
+        1nF =Cin      # input capacitance
+        50Ω =Rl       # load resistance
+    """).lstrip(),
+}
+for name, content in scripts.items():
+    with open(name, 'w') as f:
+        f.write(content)
+
 
 # Test scripting {{{1
 def test_scripting():
