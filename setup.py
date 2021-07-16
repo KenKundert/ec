1f63ba6cc3ecd99b8c8e809f77ebf516bd161dd6
Ken Kundert <ken@theKunderts.net>
2021-07-15 22:36:54 -0700

    nit

--- setup.py
+++ setup.py
@@ -22,15 +22,6 @@ setup(
       'License :: OSI Approved :: GNU General Public License v3 (GPLv3)',
       'Natural Language :: English',
       'Operating System :: POSIX :: Linux',
-      #'Programming Language :: Python :: 2',
-      #'Programming Language :: Python :: 2.7',
-      #   should work, but no longer tested
-      #'Programming Language :: Python :: 3',
-      #'Programming Language :: Python :: 3.3',
-      #   should work, but no longer tested
-      #'Programming Language :: Python :: 3.4',
-      #   should work, but no longer tested
-      #'Programming Language :: Python :: 3.5',
       'Programming Language :: Python :: 3.6',
       'Programming Language :: Python :: 3.7',
       'Programming Language :: Python :: 3.8',
